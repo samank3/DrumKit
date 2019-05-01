e200ccb06e3f68fa1ec3f723aae8ec7c93639ff9
Saman Khan <32426683+samank3@users.noreply.github.com>
2019-05-01 13:15:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
 
 
     switch (buttonInnerHTML) {
+
       case "w":
         var audio = new Audio('sounds/tom-1.mp3')
         audio.play();
@@ -20,15 +21,40 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
         audio.play();
         break;
 
+      case "s":
+        var audio = new Audio('sounds/tom-3.mp3')
+        audio.play();
+        break;
+
+      case "d":
+        var audio = new Audio('sounds/tom-4.mp3')
+        audio.play();
+        break;
+
+      case "j":
+        var audio = new Audio('sounds/snare.mp3')
+        audio.play();
+        break;
+
+      case "k":
+        var audio = new Audio('sounds/crash.mp3')
+        audio.play();
+        break;
+
+      case "l":
+        var audio = new Audio('sounds/kick-bass.mp3')
+        audio.play();
+        break;
 
 
-        default;
+
+      default: console.log(buttonInnerHTML);
 
 
 
     }
 
 
-    });
+  });
 
 }
